0fa9771d60ae3ffaacdd9c700d0857a416a8b411
Yoganand <yorganand@gmail.com>
2017-09-21 23:37:49 +0530

    Trying robo_browser

--- Crawler/robo_demo.py
+++ Crawler/robo_demo.py
@@ -5,11 +5,23 @@ browser = RoboBrowser(parser='html.parser')
 browser.open('http://ww2.readsnk.com/')
 
 chapterList = browser.find_all('a')
+chapter97 = ''
 
 for chapter in chapterList:
-    if '97' in chapter['href']:
-        print(chapter.text, "Chap 97 Link", chapter['href'])
-        print('----------------------')
+    if '099' in chapter['href']:
+        chapter97 = chapter#['href']
+        break
+
+if chapter97 == '':
+    print('No Link found. Exiting :(')
+    exit()
+
+browser.follow_link(chapter97)
+chapterHead = browser.find('h1', {'class': 'chapter__title'})
+print(chapterHead.text)
+
+
+
 
 # print(chapterList[0])
 # browser.follow_link('Shingeki No Kyojin Chapter 097')
