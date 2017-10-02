f72d431fa6a8df05891c291b822d7a67b893a8d1
Yoganand <yorganand@gmail.com>
2017-10-02 19:27:03 +0530

    sheet_viewer.py - Update XPath

--- Crawler/sheet_viewer.py
+++ Crawler/sheet_viewer.py
@@ -1,7 +1,7 @@
 from splinter import Browser
 import time
 
-# Replace <<GDRIVE_FOLDER_URL>>, <<USER_MAIL>>, <<USER_MAIL>> with appropriate values.
+# Replace <<GDRIVE_FOLDER_URL>>, <<USER_MAIL>>, <<PASSWORD>> with appropriate values.
 # This pgm will goto the provided GoogleDrive URL and display the name of all its contents
 
 DRIVE_FLDR_LINK = '<<GDRIVE_FOLDER_URL>>'
@@ -16,12 +16,14 @@ if 'accounts.google.com/signin' in browser.url:
     user_next.click()
 
     time.sleep(5)  # To make sure all elements are loaded before searching using XPath
-    pwd = browser.find_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')[0]
+    # pwd = browser.find_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')[0]
+    pwd = browser.find_by_xpath('//input[@name="password"]')[0]
     pwd_next = browser.find_by_xpath('//*[@id="passwordNext"]')[0]
 
     pwd.fill('<<PASSWORD>>')
     pwd_next.click()
 
+time.sleep(20)  # To make sure all elements are loaded before searching using XPath
 folder_items = browser.find_by_xpath('//span[@class="l-Ab-T-r"]')
 for item in folder_items:
     print(item.text)
